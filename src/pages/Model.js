221f6d86d638ebe7aed404a5fb0dcf36c2d3c0cd
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2025-07-22 16:23:54 -0400

    Add return home link

--- src/pages/Model.js
+++ src/pages/Model.js
@@ -37,7 +37,27 @@ export default function ModelPage() {
   }, []);
 
   return (
-    <div style={{ height: "100vh", background: "#111", overflow: "hidden" }}>
+      <div style={{ height: "100vh", background: "#111", overflow: "hidden" }}>
+      {/* Return Home Link */}
+      <div
+        onClick={() => navigate("/")}
+        style={{
+          position: "absolute",
+          top: "20px",
+          left: "20px",
+          color: "white",
+          cursor: "pointer",
+          fontSize: "18px",
+          display: "flex",
+          alignItems: "center",
+          gap: "6px",
+          zIndex: 10
+        }}
+      >
+        <span style={{ fontSize: "20px" }}>←</span> Return Home
+      </div>
+
+      {/* 3D Canvas */}
       <Canvas style={{ height: "100vh" }} camera={{ position: [0, 0, 5] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
